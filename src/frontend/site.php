3597cd6df12dcdeeb1246710627fff3c115bf31f
dvang315 <dv37@uakron.edu>
2023-03-12 22:19:05 -0400

    Add frontend files

---
+++ src/frontend/site.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>SEO-UP</title>
+        <link rel = "stylesheet" type = "text/css" href = "style.css" />
+    </head>
+    <body>
+        <div id = "container">
+
+            <div id = "header">
+                <h1>SEO-UP</h1>
+            </div>
+
+            <div id = "content">
+
+                <div id = "main">
+                    <h3>Improve your website today!</h3>
+                    <form action = "" class = "searchBar">
+                        <input type = "text" placeholder = "Enter website url">
+                        <button type = "submit">
+                            <span class = "button__text">Enter</span>
+                    </form>
+                </div>
+
+                <div id = "results">
+                    <h3>RESULTS</h3>
+                    <div id = "keywords">
+
+                    </div>
+                </div>
+            </div>
+
+            <div id = "footer">
+                Copyright &copy; 2023 SEO-UP Corporations
+            </div>
+        </div>
+
+
+    </body>
+</html>
+
